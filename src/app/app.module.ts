82fc8cde393bd91a98968e8dd0ead178c2de4a35
Yaswanth <42815019+yaswanthj@users.noreply.github.com>
2020-11-30 18:15:48 +0530

    Angular setup
    Angular Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './app/modules/login/login.module';
+import { DashboardModule } from './app/modules/dashboard/dashboard.module';
+import { CustomerModule } from './app/modules/customer/customer.module';
+import { OrderModule } from './app/modules/order/order.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    DashboardModule,
+    CustomerModule,
+    OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
